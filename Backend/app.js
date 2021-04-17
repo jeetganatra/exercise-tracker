ba830cf410771d95fddd9054b21995c3bd1b8ef2
jeetganatra <jeetganatra138@gmail.com>
2021-04-17 18:29:36 +0530

    Added the get and post apis for users and exercises

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+const exercisesRouter = require("./routes/exercise-routes");
+const usersRouter = require("./routes/user-routes");
 
 const app = express();
 
@@ -20,6 +22,9 @@ const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("DB successfully connected");
 });
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
   console.log("Server is up and running at port 5000");
