c7818a11f4afbef554d50ed72e99767167d787fd
ishanc008 <ishanc008@gmail.com>
2021-04-17 17:26:10 +0530

    Added create user component without API call

--- Frontend/src/Components/CreateUserComponent.js
+++ Frontend/src/Components/CreateUserComponent.js
@@ -1,9 +1,39 @@
 import React from "react"
 
 const CreateUserComponent = () => {
+    const [userName,setUserName] = React.useState("");
+
+    const handleOnChange = (event) =>{
+        const newUser = event.target.value;
+        setUserName(newUser);
+    }
+
+    const handleOnSubmit = (event) => {
+        event.preventDefault();
+        const newAddedUser = {
+            username:userName
+        }
+        axios.post("http://localhost:5000/user/add",newAddedUser)
+             .then(()=>{
+                 //set ALERT to true later here
+                 setUserName("");
+                 console.log("new user added");
+             })
+             .catch(err => {
+                console.log(err);
+            });
+    }
+
     return(
         <div>
-            <p>you are on the create user page</p>
+            <h1>Create new User here</h1>
+            <form onSubmit={handleOnSubmit}>
+                <div class="form-group">
+                    <label for="email">Email address:</label>
+                    <input type="email" class="form-control" id="email" placeholder="Enter a user name" onChange={handleOnChange} value={userName}></input>
+                </div>
+                <button type="submit" class="btn btn-dark">Submit</button>
+            </form>
         </div>
     )
 }
